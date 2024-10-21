6ab8f54f4026093d1711c3601b7d6f78e1ec0d16
lidiia <lidiiadolfin@gmail.com>
2024-10-21 11:13:10 +0300

    first commit

---
+++ js/script1.js
@@ -0,0 +1,31 @@
+import { info, success, error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+
+const arrKeys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'] 
+let currentKeyIndex 
+
+const keyElement = document.querySelector('#key')
+ 
+const generateKey = () => {
+    currentKeyIndex = Math.floor(Math.random() * arrKeys.length)
+    keyElement.textContent = arrKeys[currentKeyIndex]
+
+}
+
+window.addEventListener('keydown', (e) => {
+    e.preventDefault()
+    const pressedKey = e.key.toLowerCase()
+    if(pressedKey === arrKeys[currentKeyIndex]) {
+        success({text: 'True👍'})
+        generateKey()
+    } else {
+        error({text: 'False🫤'})
+    }
+})
+
+const gameBtn = document.querySelector('.start')
+gameBtn.addEventListener('click', () => {
+    info({text: 'The game started😊'})
+    generateKey()
+})
